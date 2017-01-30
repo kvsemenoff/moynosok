f2969e7111bba63689aa81cc4b049af815d10a66
Konstantin Semenov <kvsemenoff@mail.ru>
2017-01-30 17:57:02 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+	<?php require_once('includes/header.php'); ?>
+	<?php require_once('includes/header_top.php'); ?>
+
+	<?php require('includes/kupi.php'); ?>
+	<?php require_once('includes/catalog.php'); ?>
+	<?php require_once('includes/delivery.php'); ?>
+	<?php require('includes/kupi.php'); ?>
+	<?php require_once('includes/gifts.php'); ?>
+	<?php require_once('includes/how.php'); ?>
+	<?php require_once('includes/chestno.php'); ?>
+	<?php require_once('includes/faq.php'); ?>
+	<?php require('includes/kupi.php'); ?>
+
+	<?php require_once('includes/write.php'); ?>
+	<?php require_once('includes/footer.php'); ?>
+
+	
