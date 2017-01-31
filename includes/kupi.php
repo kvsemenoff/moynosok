03210c49a939b7c68851955c2f009a5c56387956
Berzeg07 <berezg86@gmail.com>
2017-01-31 17:14:38 +0300

    29.14

--- includes/kupi.php
+++ includes/kupi.php
@@ -1,9 +1,29 @@
-<section class="section">
+<section class="section dd-kupi-wrap">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<h2></h2>
+				<div class="dd-kupi-title">
+					<h2>Купи кейс с носками сегодня и получи 3 подарка</h2>
+				</div>
 			</div>
+			<div class="clearfix"></div>
+			<div class="col-md-5">
+				<div class="dd-kupi-keys">
+					<div class="actsia">
+						<span>Внимание! Акция!</span>
+					</div>
+					<img class="img-responsive" src="img/k.png" alt="">
+				</div>
+			</div>
+			<div class="col-md-7">
+				<div class="dd-box1">
+					<div class="dd-box-title">
+						<span>К каждому заказу</span>
+					</div>
+					<img src="img/" alt="">
+				</div>
+			</div>
+			<div class="clearfix"></div>
 		</div>
 	</div>
 </section>
