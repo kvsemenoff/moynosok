39dfe3532692e98f714758eb001e45f9baee9283
Berzeg07 <berezg86@gmail.com>
2017-02-01 11:31:28 +0300

    29.14

--- includes/kupi.php
+++ includes/kupi.php
@@ -1,3 +1,4 @@
+
 <section class="section dd-kupi-wrap">
 	<div class="container">
 		<div class="row">
@@ -7,15 +8,46 @@
 				</div>
 			</div>
 			<div class="clearfix"></div>
-			<div class="col-md-5">
+			<div class="col-lg-5">
 				<div class="dd-kupi-keys">
 					<div class="actsia">
 						<span>Внимание! Акция!</span>
 					</div>
-					<img class="img-responsive" src="img/k.png" alt="">
+					<div class="dd-img-respons">
+						<img class="img-responsive" src="img/k.png" alt="">
+					</div>
+					<div class="dd-img-respons2">
+						<img src="img/k.png" alt="">
+					</div>
 				</div>
+
+				<!-- Таймер -->
+				<div class="dd-timer-all-wrap">
+					<div class="dd-timer">
+						<div class="dd-timer-title">
+							<span>до конца акции осталось</span>
+						</div>
+						<div class="dd-times">
+							<div class="dd-div">
+								<span id="dd-hours"></span><br>
+								<span class="time-txt">Часов</span>
+							</div>
+							<div class="dd-div dd-div-margin">
+								<span id="dd-min"></span><br>
+								<span class="time-txt">Минут</span>
+							</div>
+							<div class="dd-div dd-div-margin">
+								<span id="dd-sec"></span><br>
+								<span class="time-txt">Секунд</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- Конец Таймер -->
+
+				<div class="clearfix"></div>
 			</div>
-			<div class="col-md-7">
+			<div class="col-lg-7">
 				<div class="dd-box1 m-left">
 					<div class="dd-box-title">
 						<span>К каждому заказу</span>
@@ -31,13 +63,30 @@
 					</div>
 					<img class="img-responsive" src="img/ddimg2.png" alt="">
 					<span class="vpodarok">в подарок</span><br>
-					<span class="bottom-txt">– 2 пары войлочных</span><br>
-					<span class="bottom-txt">фольгированных стелек</span>
+					<span class="bottom-txt">2 пары стелек</span><br>
+					<span class="bottom-txt">+ портмоне CarWallet</span>
 				</div>
 				<div class="clearfix"></div>
+				<div class="dd-box1 dd-box2 dd-box3">
+					<div class="dd-box-title">
+						<span>Свыше 2990</span>
+					</div>
+					<div class="dd-box3-element">
+						<img class="img-responsive" src="img/ddimg3.png" alt="">
+					</div>
+					<div class="dd-box3-element dd-box3-element2">
+						<span class="vpodarok">в подарок</span><br>
+						<span class="bottom-txt">2 пары стелек</span><br>
+						<span class="bottom-txt">+ портмоне</span><br>
+						<span class="bottom-txt">CarWallet</span><br>
+						<span class="bottom-txt">и часы Curren</span>
+					</div>
+					<div class="clearfix"></div>
+				</div>
 			</div>
 			<div class="clearfix"></div>
 		</div>
 	</div>
 </section>
 <?php require('forms.php'); ?>
+
