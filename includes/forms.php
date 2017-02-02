30509ac615e3d1d450e92e7eae528d23400f9a5b
ibragimtokmak <tokmakovibragim@gmail.com>
2017-02-02 11:52:37 +0300

    frffd

--- includes/forms.php
+++ includes/forms.php
@@ -1,7 +1,7 @@
 <div class="popup__form">
     <form action="thanks.php" method="post" class="form1">
         <input type="text" placeholder="Ваше Имя" name="uname" required>
-        <input type="text" placeholder="Ваш Email" name="email" required>
+        <input type="text" placeholder="Ваш Телефон" name="phone" class="phone" required>
         <input type="submit" name="submit" placeholder="Оставить заявку">
     </form>
 </div>
